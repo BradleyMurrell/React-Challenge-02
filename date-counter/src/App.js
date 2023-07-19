9a01c2b35c8cb3cb343a25ea0234e5284c8f622a
BradleyMurrell <95315836+BradleyMurrell@users.noreply.github.com>
2023-07-19 21:05:15 +0000

    Add a step counter

--- date-counter/src/App.js
+++ date-counter/src/App.js
@@ -10,6 +10,7 @@ export default function App() {
 
 function Counter() {
   const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
 
   const date = new Date();
   date.setDate(date.getDate() + count);
@@ -17,12 +18,29 @@ function Counter() {
   return (
     <div>
       <div>
-        <button onClick={() => setCount((c) => c - 1)}>-</button>
-        <span>Count: {count}</span>
-        <button onClick={() => setCount((c) => c + 1)}>+</button>
+        <button onClick={() => setStep((c) => c - 1)}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={() => setStep((c) => c + 1)}>+</button>
       </div>
 
-      <p>{date.toDateString()}</p>
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count === 1
+            ? `${count} day from today is `
+            : count > 1
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
     </div>
   );
 }
